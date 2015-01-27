8ce3b2c599f0446da1ba8bf2c73c2c7b5fcf8e7d
dltkr77 <is77@mobigen.com>
2015-01-27 17:27:21 +0900

    Add TimebaseCounter

--- weblog/src/main/java/weblog/TimebaseCounter.java
+++ weblog/src/main/java/weblog/TimebaseCounter.java
@@ -1,41 +1,35 @@
 package weblog;
 
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-public class AttackDetector extends Configured implements Tool {
+public class TimebaseCounter extends Configured implements Tool {
 	public static void main(String[] args) throws Exception {
 		if(args.length != 2) {
-			System.out.println("Usage: AttackDetector <input dir> <output dir>");
+			System.out.println("Usage: TimebaseDetector <input dir> <output dir>");
 			System.exit(-1);
 		}
-		
-		int result = ToolRunner.run(new Configuration(), new AttackDetector(), args);
+		int result = ToolRunner.run(new Configuration(), new TimebaseCounter(), args);
 		System.exit(result);
 	}
 
 	public int run(String[] args) throws Exception {
 		Configuration conf = getConf();
-		Job job = new Job(conf, "Attack Detector");
+		Job job = new Job(conf, "Timebase Detector");
 		
-		job.setJarByClass(AttackDetector.class);
-		job.setMapperClass(AttackDetectorMapper.class);
-		job.setReducerClass(AttackDetectorReducer.class);
+		job.setJarByClass(TimebaseCounter.class);
+		job.setMapperClass(TimebaseMapper.class);
+		job.setReducerClass(TimebaseReducer.class);
 		
 		job.setMapOutputKeyClass(Text.class);
 		job.setMapOutputValueClass(Text.class);
@@ -43,56 +37,47 @@ public class AttackDetector extends Configured implements Tool {
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
 		
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		
 		boolean success = job.waitForCompletion(true);
 		return success ? 0 : 1;
 	}
 	
-	public static class AttackDetectorMapper extends Mapper<LongWritable, Text, Text, Text> {
-		private final static String regex = 
-				"(\\w+=\\w+)(\\;|%20|\'|\"|\\s+|[0-9]+)(and|or|exec|and|or|create|select)";
-		private Pattern p = Pattern.compile(regex);
+	public static class TimebaseMapper extends Mapper<LongWritable, Text, Text, Text> {
 		/*
          * () == key
 		 * Input : (offset), IP, Time, URL, Parameter, Status
-		 * Output : (IP, Time), URL, Parameter, Status, 1[]
+		 * Output : (IP, Time), URL, Parameter, Status
 		 */
 		@Override
 		protected void map(LongWritable key, Text value,
 				Mapper<LongWritable, Text, Text, Text>.Context context)
 				throws IOException, InterruptedException {
-			String words[] = value.toString().split("\\s+");
+			String[] words = value.toString().split("\\s+");
 			String ip = words[0];
 			String time = words[1];
 			String url = words[2];
 			String param = words[3];
 			String stat = words[4];
 			
-			Matcher m = p.matcher(param);
-			if(m.find() || param.contains("xp_cmdshell")) {
-				context.write(new Text(ip + " " + time), new Text(url + " " + param + " " + stat)); // 1 skip
-			}
+			context.write(new Text(ip + " " + time), new Text(url + " " + param + " " + stat));
 		}
 	}
 	
-	public static class AttackDetectorReducer extends Reducer<Text, Text, Text, IntWritable> {
+	public static class TimebaseReducer extends Reducer<Text, Text, Text, IntWritable> {
 		/*
          * () == key
-		 * Input : (IP, Time), URL, Parameter, Status, 1[] -- 1 skip
-		 * Output : (IP, Time), Probability
+		 * Input : (IP, Time), URL, Parameter, Status
+		 * Output : (IP, Time), count
 		 */
 		@Override
 		protected void reduce(Text key, Iterable<Text> values,
 				Reducer<Text, Text, Text, IntWritable>.Context context)
 				throws IOException, InterruptedException {
-								
-			int prob = 0;
+			
+			int count = 0;
 			for(Text value : values) {
-				prob++;
+				count++;
 			}
-			context.write(key, new IntWritable(prob));
+			context.write(key, new IntWritable(count));
 		}
 	}
 }
