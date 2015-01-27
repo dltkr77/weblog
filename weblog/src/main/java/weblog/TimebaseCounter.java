9541e55ff6ccd2fcf6f0fc115b178ab8cf4b2d5f
dltkr77 <is77@mobigen.com>
2015-01-27 17:33:24 +0900

    modify

--- weblog/src/main/java/weblog/TimebaseCounter.java
+++ weblog/src/main/java/weblog/TimebaseCounter.java
@@ -4,12 +4,15 @@ import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -36,6 +39,9 @@ public class TimebaseCounter extends Configured implements Tool {
 		
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
+		
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		
 		boolean success = job.waitForCompletion(true);
 		return success ? 0 : 1;
